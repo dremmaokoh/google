77726f0407f4be8e5e2b07b04fe3d5180f44115b
Pipemma <dremmaokoh@gmail.com>
2023-05-26 23:41:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Google
+A simple google authentication using passport-google-oauth2 
+
+Stack Used
+```````````
+>- Vscode : Work environment
+>- Nodejs : Open source
+
+```````
+Packages Installed
+````````````
+>- Express : Framework
+>- express-session : Create a session middleware
+>- passport-google-oauth2 : authenticate using Google
+>- passport : to authenticate requests
+>- Dotenv : Hide my keys
+>- Nodemon : Restart the server
+
+`````````````
+
+
+``````````````
+How to install
+``````````````
+>- Clone this repository
+>- Run : <kbd>npm i </kbd>
+>- Run: <kbd>npm start <kbd>
+
+````````````````````
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require ('express');
+const passport = require ('passport')
+require("dotenv").config();
+const app = express ();
+const session = require('express-session');
+app.use(
+    session({
+    secret: process.env.KEYS,
+    resave: false,
+    saveUninitialized: false,
+   cookie: { httpOnly: true,
+             secure: false,
+             maxAge: 24 * 60 * 60 * 1000,
+  }
+  }))
+  app.use (passport.initialize());
+  app.use (passport.session());
+require ('./auth');
+require("dotenv").config();
+
+//function to check if our user is logged in via middleware
+function isLoggedIn(req, res, next){
+    req.user ? next() : res.sendStatus (401);
+}
+//we want this user to become part of the request when someone logs in, we use espress-session for session management
+
+app.get('/', (req, res)=>
+{res.send ( '<a href="/auth/google">Authenticate with Google</a>');
+});
+//declaring what happens with the link above
+//scope is what kind of information do you want to retrive from the customer's profile 
+app.get ('/auth/google',
+             passport.authenticate('google', {scope : ['email', 'profile']}));
+
+//to take care of callaback
+app.get('/google/callback',
+passport.authenticate ('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+})
+)
+
+// to handle auth/failure
+app.get('/auth/failure', (req, res) => {
+    res.send ('something went wrong .....')
+});
+
+
+
+app.get ('/protected', isLoggedIn, (req, res) =>{
+res.send(`Hello ${req.user.displayName}`)
+});
+
+
+app.get ('/logout', (req, res) => {
+    res.clearCookie("access_token");
+    req.session.destroy();
+    return res.send ('Goodbye!!!!!!');
+  });
+
+
+ app.listen (9450, () => console.log ('listening on : 9450'));
