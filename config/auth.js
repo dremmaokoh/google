6404859902aa204e00e241bea3ba5990e247e7f9
Pipemma <dremmaokoh@gmail.com>
2023-06-06 22:00:07 +0100

    updated logic

--- config/auth.js
+++ config/auth.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://google-three-flax.vercel.app/google/callback",
+      callbackURL: "/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       console.log(profile);
